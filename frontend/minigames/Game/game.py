2f27d60fbc9ab773d248e4ae915b6d9102d6eb7a
Alex <soloma424@gmail.com>
2025-11-04 10:28:37 -0700

    creating simple OOP

--- frontend/minigames/Game/game.py
+++ frontend/minigames/Game/game.py
@@ -30,42 +30,41 @@ class Game:
 
         self.clock = pygame.time.Clock()
         self.running = True
+    def handle_input(self):
+        """Handle user input for player movement."""
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.player_start.move(0, -1, self.maze)
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.player_start.move(0, 1, self.maze)
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.player_start.move(-1, 0, self.maze)
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.player_start.move(1, 0, self.maze)
 
-        def handle_input(self):
-            """Handle user input for player movement."""
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP] or keys[pygame.K_w]:
-                self.player_start.move(0, -1, self.maze)
-            elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                self.player_start.move(0, 1, self.maze)
-            elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
-                self.player_start.move(-1, 0, self.maze)
-            elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-                self.player_start.move(1, 0, self.maze)
+    def check_win(self):
+        """Check if the player has reached the exit."""
+        # Ensure we don't index out of bounds; the maze layout should be consistent.
+        if self.maze_layout[self.player_start.pos_y][self.player_start.pos_x] == 'E':
+            print("You Win!")
+            self.running = False
 
-        def check_win(self):
-            """Check if the player has reached the exit."""
-            if self.maze.layout[self.player_start.y][self.player_start.x] == 'E':
-                print("You Win!")
-                pygame.time.wait(1000)
-                self.running = False
+    def run(self):
+        """Main game loop."""
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-        def run(self):
-            """Main game loop."""
-            while self.running:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        self.running = False
+            self.handle_input()
+            self.check_win()
 
-                self.handle_input()
-                self.check_win()
+            self.screen.fill(BLACK)
+            self.maze.draw(self.screen)
+            self.player_start.draw(self.screen)
 
-                self.screen.fill(BLACK)
-                self.maze.draw(self.screen)
-                self.player_start.draw(self.screen)
-                
-                pygame.display.flip()
-                self.clock.tick(FPS)
+            pygame.display.flip()
+            self.clock.tick(FPS)
 
-            pygame.quit()
-            sys.exit()
+        pygame.quit()
+        sys.exit()
